d36054cb2104a3b53c29c4ff1821bfb82fca9bee
cassfer <cassiohafernandes@gmail.com>
2024-01-17 01:11:43 -0300

    CQCB - Added health check

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHealthChecks("/health");
 
 app.UseHttpsRedirection();
 
